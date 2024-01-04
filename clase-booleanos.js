488dcdbd38d35b9edad4520260f765d5d416e837
Israel Garcia <isragarciahdez@gmail.com>
2024-01-04 13:16:21 -0600

    Trabajando con boolean, null, undefined, symbol y bigint

---
+++ clase-booleanos.js
@@ -0,0 +1,14 @@
+const isActive = true;
+const hasPermission = false;
+
+//Conversión implícita
+const result = 5 > 3;
+console.log(result);
+
+const name = 'Platzi';
+console.log(!!name);
+
+//Conversión explícita
+const value = 0;
+const explicitBoolean = Boolean(value);
+console.log(explicitBoolean);
